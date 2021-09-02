ae2574337c1f82fc5fa8888e1d7f59bd1a0dc699
Dkoztai <84252148+Dkoztai@users.noreply.github.com>
2021-09-02 02:17:06 -0400

    Add files via upload

--- ABootcampProject/src/test/java/tt/runners/RunnerClass.java
+++ ABootcampProject/src/test/java/tt/runners/RunnerClass.java
@@ -9,10 +9,10 @@ import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = { "src/test/resource/FeaturesFiles" }, glue = { "tt.ebay.stepDef",
+@CucumberOptions(features = { "src/test/resource/FeatureFiles" }, glue = { "tt.ebay.stepDef",
 		"tt.utilities" }, tags = { "@regression" }, plugin = { "pretty", "html:target/cucumber-htmlreport",
 				"json:target/cucumber-report.json",
-				"com.cucumber,listener.ExtentCucumberFormatter:target/cucumber-reports.html" }, monochrome = true)
+				"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports.html" }, monochrome = true)
 
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
