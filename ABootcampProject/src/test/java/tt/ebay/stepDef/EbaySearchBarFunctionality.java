fdf9dfec61fa1638b23a57918604ec18c76295f4
Dkoztai <84252148+Dkoztai@users.noreply.github.com>
2021-09-02 02:21:03 -0400

    Add files via upload

---
+++ ABootcampProject/src/test/java/tt/ebay/stepDef/EbaySearchBarFunctionality.java
@@ -0,0 +1,31 @@
+package tt.ebay.stepDef;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import tt.ebay.pageAction.EbayHomePageActions;
+import tt.ebay.pageAction.EbaySearchBarResultActions;
+
+public class EbaySearchBarFunctionality {
+	
+	EbayHomePageActions EbayHomePageActionsObj = new EbayHomePageActions();
+	EbaySearchBarResultActions EbaySearchBarResultActionsObjs= new EbaySearchBarResultActions();
+	
+
+	@When("^click search bar$")
+	public void click_search_bar() throws Throwable {
+		EbaySearchBarResultActionsObjs.searchbar();
+	}
+
+	@When("^input baby$")
+	public void input_baby() throws Throwable {
+		EbaySearchBarResultActionsObjs.babysrch();
+	}
+
+	@Then("^verify verify baby$")
+	public void verify_verify_baby() throws Throwable {
+		EbaySearchBarResultActionsObjs.veryfiybabysrch();
+	}
+
+
+}
